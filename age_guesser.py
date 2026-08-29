086cf7336f183f62e2a7e2ccdcd993f911e859d6
edwardfishhands <syd.baker223@gmail.com>
2026-08-29 11:30:19 -0600

    first commit

---
+++ age_guesser.py
@@ -0,0 +1,16 @@
+import random
+
+def guesser():
+    print("Hello! I'm going to try to guess your age!\n")
+    name = input("What is your name?\n")
+    guess = False
+    while !guess:
+        age = random.randint(15,40)
+        user = input(f"Are you {age} years old? (y/n)\n")
+        if user = "n":
+            print("Rats!")
+        else:
+            print(f"{user} is {age} years old")
+            guess = True
+    
+guesser()
