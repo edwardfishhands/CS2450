6e38ba7321d07f5b61587f093e894e9dc79982fc
edwardfishhands <syd.baker223@gmail.com>
2026-08-29 11:33:33 -0600

    changed sytax for while loop and fixed if statement

--- age_guesser.py
+++ age_guesser.py
@@ -4,10 +4,10 @@ def guesser():
     print("Hello! I'm going to try to guess your age!\n")
     name = input("What is your name?\n")
     guess = False
-    while !guess:
+    while guess == False:
         age = random.randint(15,40)
         user = input(f"Are you {age} years old? (y/n)\n")
-        if user = "n":
+        if user == "n":
             print("Rats!")
         else:
             print(f"{user} is {age} years old")
